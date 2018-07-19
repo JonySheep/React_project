41e575b93405310b4edceb5af8ee002fba39af3c
Yang <161250180@mail.smal.nju.edu.cn>
2018-07-19 20:44:45 +0800

    终于能跑了！！！

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {Hello} from "./component/Hello";
-
-ReactDOM.render(
-    <Hello name={'刘昊然'} age={18}/>,
-    document.getElementById('app')
-);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,64 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+
+var basePath = __dirname;
+
 module.exports = {
-    entry:"./src/index.tsx",
-    output:{
-        filename: "app.js",
+    context: path.join(basePath, "src"),
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
     },
-    devtool:"source-map",
-    resolve:{
-        extensions: [".ts", ".tsx", ".js"]
+    entry: [
+        './main.ts',
+        '../node_modules/bootstrap/dist/css/bootstrap.css'
+    ],
+    output: {
+        path: path.join(basePath, 'dist'),
+        filename: 'bundle.js'
     },
-    module:{
-        rules:[
-            {test:/\.tsx?$/,loader:"ts-loader"}
-        ]
-    }
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist', // Content base
+        inline: true, // Enable watch and live reload
+        host: 'localhost',
+        port: 8080,
+        stats: 'errors-only'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                loader: 'awesome-typescript-loader',
+                options: {
+                    useBabel: true,
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/img/[name].[ext]?[hash]'
+                }
+            },
+        ],
+    },
+    plugins: [
+        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html', //Name of file in ./dist/
+            template: 'index.html', //Name of template in ./src
+            hash: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+    ],
 };
